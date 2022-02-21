aa2b7fff2dd5a3f2d270ff0a6d71ac1c4526117b
Saneesh <cssaneesh@gmail.com>
2022-02-21 10:56:53 +0100

    final for homework

--- fruit_plant.R
+++ fruit_plant.R
@@ -1,51 +1,253 @@
 
-plants <- read.csv("fruiting_plants.csv")
+# install.packages("tidyverse")
+library(tidyverse) # or 
+library(dplyr) # if you have installed tidyverse you can use dplyr because dplyr is part of tidyverse
 
+# data frame----
+?data.frame # to know more about data frame
+# let us make a data frame before we go to plant data
+
+name <- c("saneesh", "sanusha", "Appu", "Kuttan", "Neeraja") # here name is one column name
+sex <- c(1,2,1, 1, 2) # here sex is one column name
+age <- c(40, 33, 8, 48, 32) # here age is one column name
+height <- c("tall", 'short', 'tall', 'tall', 'short')
+weight <- c(62, 48, 20, 72, 60)
+# we made three vectors
+
+typeof(name)
+class(name)
+
+# let us make a data frame
+mydf <- data.frame(name,sex,age, height) # mydf is the short form I gave for my data frame :)
+
+# to understand content of the data frame we can use the following
+mydf
+mydf %>% view() # adding view () will open a new tab and pipe %>% can only be used if you are using tidyverse or dplyr
+str(mydf)
+glimpse(mydf) # glimpse () can only be used if you are using tidyverse or dplyr
+summary(mydf)
+
+# select----
+# let us check the variable column age
+mydf %>% select(age)
+# homework----
+# try with other variables 
+
+mydf %>% select(x) # replace x with a variable in the data frame
+
+# we can select multiple variables
+
+mydf %>% select(name, age)
+# homework----
+mydf %>% select(x, x) # replace x with another variables
+
+mydf %>% select(-age) # here except age all others are selected
+
+mydf %>% select(sex:height) # here coloums starting from sex to height is selected
+
+# spread of data----
+mydf %>% select(age) %>% summarise(min(age)) # min= minimum
+mydf %>% select(age) %>% summarise(max(age)) # max= maximum
+mydf %>% select(age) %>% summarise(sd(age)) # sd= standard deviation 
+mydf %>% select(age) %>% summarise(mean(age)) # mean= average
+
+# we can do everything together
+mydf %>% select(age) %>% summarise(min(age), max(age), sd(age), mean(age))
+
+# mutate----
+# recode----
+
+mydf %>% select(name)
+
+
+# in the name column saneesh and sanusha begin with lower case
+# but should be upper case because those are names
+# we can recode those values by the following line of code 
+mydf %>% mutate(name=recode(name,"saneesh"= "Saneesh",
+                                    "sanusha"= "Sanusha"))
+# we used two functions at the same time, mutate and recode
+# check if it has changed.
+mydf %>% select(name)
+glimpse(mydf) # it is still lower case!!! why?
+# homework----
+# hint-- mydf <- 
+
+# check if it has changed.
+glimpse(mydf)
+
+# visualize data ----
+# exploratory graphs will help us understand data better
+
+# install.packages("tidyverse")
+library(ggplot2) # if you have installed tidyverse you can use ggplot2, ggplot is also part of tidyverse
+
+# scatter plot----
+?geom_point
+
+# do not run the following line, read it :)
+ggplot(data= fill, mapping= aes(x= fill, y= fill))+
+  geom_point() # replace fill with information we have
+
+# by convention x axis is independent variable and y axis is dependent variable
+# we are assuming that weight gain with age.
+# or weight is dependent on age
+
+# push the following command to check
+ggplot(data= mydf, mapping= aes(x= age, y= weight))+
+  geom_point()
+# click on zoom on the Plots window to see it big
+
+# homework----
+# write what do you think of relationship with age and weight from the graph?
+
+# box plot----
+
+ggplot(data= mydf, mapping = aes(y=weight))+
+  geom_boxplot()
+# this is a summary of weight in our data frame
+# here we can see min, max, mean and IQR of weight
+
+# let us compare weight of sex 
+ggplot(data= mydf, mapping = aes(y=weight, x= sex))+
+  geom_boxplot()
+
+# above command made a plot but not really useful
+# because 
+glimpse(mydf)
+# sex is understood as double or number
+# we have to tell r this is not a number but a factor
+?factor
+
+mydf <- mydf %>% mutate(sex=as.factor(sex))
+# homework----
+# which ones are functions and which are variables
+
+glimpse(mydf)
+
+# same code below the note
+# let us compare weight of sex 
+ggplot(data= mydf, mapping = aes(y=weight, x= sex))+
+  geom_boxplot()
+
+# I think instead of 1, 2 = Male and Female are good names for categorizing sex
+# 4. homework----
+# recode 1 and 2 to Male and Female
+# 5. homework----
+# make a box x= sex and y= height
+
+# filter----
+
+# let us filter only Female entries from the data frame
+
+
+
+mydf %>% filter(sex== 2) # If you changed the name earlier, use the changed name.
+mydf %>% filter(sex== Female)
+
+# more filter----
+
+# filter all short people
+mydf %>% filter(height== "short")
+
+# filter people older than 32
+
+mydf %>% filter(age>32)
+
+# barplot----
+
+ggplot(mydf, aes(x= sex))+
+  geom_bar()
+
+ggplot(mydf, aes(x= sex, fill= sex))+
+  geom_bar() # added a new feature to graph to make it good looking!!
+
+# homework----
+# make a barplot on the x axis height, fill sex
+
+# now if you want to save the plot as pdf, jpg, png, etc
+# assign the plot to a name
+
+myfirstplot <- ggplot(mydf, aes(x= height, fill= height))+
+  geom_bar()
+myfirstplot
+
+mysecondplot <- ggplot(mydf, aes(x= height, y= age, color= sex, size= age))+
+  geom_point()
+mysecondplot
+
+# them use ggsave(name of the plot)
+ggsave("myfirstplot.jpg") # this can be seen in your folder
+ggsave("mysecondplot.pdf")
+ggsave("myfirstplot.png", width = 5, height = 5, dpi = 200)
+
+# 6. homework----
+# save previous plots also the same way and share with the group
+# make more plots to get more experience and score :) !!
+
+# select and filter
+mydf %>% select(name, height, age) %>% filter(age>35) # greater than
+mydf %>% select(name, height, age) %>% filter(age>=35) # grater than or wqual to
+mydf %>% select(name, height, age) %>% filter(age<35) # less than
+mydf %>% select(name, height, age) %>% filter(age<=33) # less than or equal to
+mydf %>% select(name, height, age) %>% filter(age==35) # equal to
+mydf %>% select(name, height, age) %>% filter(age==33)# equal to
+mydf %>% select(name, height, age) %>% filter(age!=33) # not equal to 
+
+# Go to Session > Restart R or Ctrl+Shift+F10
+
+# plants----
+# let us get back to plants data set
+plants <- read.csv("fruiting_plants.csv")
 View(plants)
 
 library(tidyverse)
 
 glimpse(plants)
 
-# View(plants)
 names(plants) %>% tibble()
 
 # clean names----
-
 # let us clean column titles
-
 # install.packages('janitor')
+library(janitor) 
 
-library(janitor)
+plants <- plants %>% clean_names() # cleaned names
 
-plants <- plants %>% clean_names()
-
+names(plants) %>% view()
 names(plants) %>% tibble()
 
 # If you want use clean_names later save it 
-
 # view(plants)
 # case change----
 glimpse(plants)
+
 plants$common_name # notice that some names are in lower case, ideally a name should be in uppercase
+
+# select----
 plants %>% select(common_name)# notice that some names are in lower case, ideally a name should be in uppercase
 
 # to do this, stringr has a function, install stringr, remove the hash that is before install.package()
+
 # install.packages('stringr') 
 
 library(stringr)
+
 plants <- plants %>% 
   mutate(common_name = ifelse(str_detect(common_name, "^[:upper:]+$"), 
                               common_name, str_to_title(common_name)))
 
-# homework-----
+view(plants$have_you_seen_this_plant)
 
-# do the above for demand_in_the_market, 
-View(plants) # see if it has changed
+plants %>% select(have_you_seen_this_plant) # %>% view()
 
-# do the above for have_you_seen_this_plant
 
-View(plants) # see if it has changed
+plants <- plants %>% 
+  mutate(have_you_seen_this_plant = ifelse(str_detect(have_you_seen_this_plant, "^[:upper:]+$"), 
+                                           have_you_seen_this_plant, str_to_title(have_you_seen_this_plant)))
+
+
+view(plants)
+names(plants)
 
 # recode----
 plants %>% select(scientific_name )
@@ -64,25 +266,8 @@ plants %>% select(scientific_name)
 plants %>% select(scientific_name)
 
 # height----
+
 # there are a lots of mistakes, take care while you enter data
-plants <- plants %>% mutate(height_in_m= recode(height_in_m, 
-                                      '40'= 40,
-                                      '6'= 6,
-                                      "8-10" = 9,
-                                      '5-10' =7.5,
-                                      '5-10' =7.5,
-                                      "More than 10" =10,
-                                      "15 to 20"= 17.5,
-                                      'upto 11'= 11,
-                                      "10 to 12"= 11,
-                                      'upto 14'= 14,
-                                      "6-25" = 15.5,
-                                      "15-20" = 17.5,
-                                      "4.5-10"= 7.5,
-                                     "upto 30"= 30,
-                                     "upto 30"= 30,
-                                     "13"= 13, "10"=10
-                                      )) 
 # It is good to change data in the csv file rather than doing like this !!!
 
 plants %>% select(height_in_m)
@@ -96,56 +281,166 @@ plants %>% select(common_name, trunk_circumference_in_m )
 
 # factors----
 glimpse(plants)
+
 is.factor(plants$demand_in_the_market)
 is.factor(plants$have_you_seen_this_plant)
 # demand_in_the_market and have_you_seen_this_plant can be ordered if we want
 # to do that we need to convert the values as factors 
 plants <- plants %>% mutate(demand_in_the_market= as.factor(demand_in_the_market))
-
+is.factor(plants$demand_in_the_market)
 # following is the base r way of converting character as factor
-plants$have_you_seen_this_plant <- as.factor(plants$have_you_seen_this_plant)
+plants$demand_in_the_market <- as.factor(plants$demand_in_the_market)
 
 is.factor(plants$demand_in_the_market)
+
+# have you seen this plant
+
 is.factor(plants$have_you_seen_this_plant)
-
-levels(plants$demand_in_the_market) # because r is case sensitive (upper and lower)
-levels(plants$have_you_seen_this_plant)
-
-plants <- plants %>% mutate(demand_in_the_market=toupper(demand_in_the_market))
-plants <- plants %>% mutate(have_you_seen_this_plant=toupper(have_you_seen_this_plant))
-
-levels(plants$demand_in_the_market)
-levels(plants$have_you_seen_this_plant)
-
-plants <- plants %>% mutate(demand_in_the_market= as.factor(demand_in_the_market))
+#  conver to factor
 plants <- plants %>% mutate(have_you_seen_this_plant= as.factor(have_you_seen_this_plant))
 
-levels(plants$demand_in_the_market)
 levels(plants$have_you_seen_this_plant)
 
+is.factor(plants$have_you_seen_this_plant) # to check if a variable or a column is a factor
+is.numeric(plants$trunk_circumference_in_m) # to check if numeric
+is.numeric(plants$have_you_seen_this_plant) # same as above
+is.character(plants$name_of_your_state)
+
+# descriptive statistics ----
+
+# Descriptive statistics summarizes or describes the characteristics of a data
+# set. Descriptive statistics consists of two basic categories of measures: 
+# measures of central tendency and measures of variability (or spread)
+
+# we will make tables to summarise numeric variables
+# we will make contingency tales to summarise categorical variables
+
+# now we have a clean data
+
+# we are interested in what is happening with height of our plants
+# spread of the data----
+min(plants$height_in_m) 
+max(plants$height_in_m)
+range(plants$height_in_m)
+IQR(plants$height_in_m) # IQR may also be called the midspread, middle 50%
+# The interquartile range of an observation variable is the difference of its upper and 
+# lower quartiles. 
+# It is a measure of how far apart the middle portion of data spreads in value.
+
+# centrality of the data----
+mean(plants$height_in_m)
+median(plants$height_in_m)
+sd(plants$height_in_m)
+# variance----
+var(plants$height_in_m) # The variance is a numerical measure of how the data values is dispersed around the mean
+
+# summary----
+summary(plants)
+
+# let us select two variables and see
+plants %>% select(height_in_m, trunk_circumference_in_m) %>% 
+  summary()
+
+# we want to see demand if demand is based on color of flower color
+plants %>% group_by(demand_in_the_market, flower_color) %>% 
+  summarise() # %>% view()
+
+# homework----
+
+# see if the same is with fruit_color
+
+# barplot----
+ggplot(plants, aes(x= fruit_color))+
+  geom_bar()
+
+# homework----
+copy the code above add color (hint: fill) to demand_in_the_market
+
+# homework----
+# save the above plot as a jpg and share
+
+# What color flowers are there?
+
+plants %>% distinct(flower_color) # using pipe or tidyverse way
+unique(plants$flower_color) # base r way 
+table(plants$flower_color)
+table(plants$flower_color) %>% tibble()
+
+plants %>% distinct(fruit_color)
+
+
+plants <- plants %>% mutate(flower_color=recode(flower_color, 'yellow'='yellow' ))
+
+plants %>% select(flower_color) %>% 
+  distinct()
+
+# select----
 plants %>% 
-    select(common_name)
-# toupper(plants$Common.name) %>% view()
+  select(common_name)
+
+# filter----
+
+# select plants which have small, let us say less than 10 meter
+plants %>% select(common_name, height_in_m) %>% 
+  filter(height_in_m<10) # less than 10 meter tall
+
+# equal and less than 10 meter  
+plants %>% select(common_name, height_in_m) %>% 
+  filter(height_in_m<=10)
+
+plants %>% select(common_name, height_in_m) %>% 
+  filter(height_in_m>25)
+
+# group----
+# demand in the market
+plants %>% group_by(demand_in_the_market) %>% 
+  summarise(number=n())
+
+# flower color
+plants %>% 
+  group_by(flower_color) %>% 
+  summarise(Lower= min(number_of_products),
+            Average= mean(number_of_products),
+            Upper= max(number_of_products),
+            Range= max(number_of_products)- min(number_of_products)) %>% 
+  arrange(Average) # %>% view()
+
+ggplot(plants, aes(flower_color, number_of_products))+
+  geom_bar(stat = "identity")
+
+ggplot(plants, aes(flower_color, number_of_countries))+
+  geom_bar(stat = "identity")
+
+# select----
+plants %>% 
+  select(common_name)
+
+boxplot(plants$height_in_m) # base r way
 
 plants %>% 
   summarise(height_in_m)
+# arrange----
+# order-----
+plants %>% arrange(height_in_m) # order height in ascending order
+plants %>% arrange(desc(height_in_m)) # order height in descending order
 
-plants %>% arrange(height_in_m) %>% view()
+# homeowork----
+# order other numeric variables and character variables
 
 plants %>% group_by(name_of_your_state) %>% 
-  count(Common_name)
+  count(common_name)
 
-ggplot(plants, aes(name_of_your_state, fill= Common_name))+
+ggplot(plants, aes(name_of_your_state, fill= common_name))+
   geom_histogram(stat = "count")
 
-ggplot(plants, aes(Common_name,height_in_m, size=height_in_m, color=Common_name ))+
+ggplot(plants, aes(common_name,height_in_m, size=height_in_m, color=common_name ))+
   geom_point()
 
 ggplot(plants, aes(height_in_m , trunk_circumference_in_m))+
   geom_point()+
   geom_smooth(method = "lm")
 
-ggplot(plants, aes(common_name, Varities, size=number_of_countries))+
+ggplot(plants, aes(common_name, varities, size=number_of_countries))+
   geom_point()+ # i dont think apple is only in xx countries
 coord_flip() # typeof(plants$Number.of.countries), re code to a meaningful number
 
@@ -156,9 +451,9 @@ ggplot(plants, aes(x=common_name, y= energy_kcal, fill=number_of_products))+
 ggplot(plants, aes(x=number_of_countries, y= number_of_products, 
                    col=common_name, size= energy_kcal))+ geom_point()
 
-
-now we want to know some number i.e
-plants %>% select(common_name, number_of_products) %>% filter(number_of_products>5) %>% tibble() 
-  
+# do all homeworks, no matter if there are mistakes but do it.
+# add your doubts as comments 
+# mail it to me
+# thank you---
 
 
